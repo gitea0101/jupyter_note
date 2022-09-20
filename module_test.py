b025b5139ea0779f0e1f8a596161b81a7b8317d9
gitea0101 <skarndrlxo@naver.com>
2022-09-20 10:41:03 +0900

    ㅁㅁ

---
+++ module_test.py
@@ -0,0 +1,21 @@
+# %%
+def add(a,b):
+    c = a + b
+    return c
+
+# %%
+def minus(a,b):
+    c = a - b
+    return c
+
+# %%
+def times(a,b):
+    c = a * b
+    return c
+
+# %%
+def de(a,b):
+    c = a / b
+    return c
+
+
